081f221eda188aa0c478d0a86ced8c11c81c9041
Shaohua Huang <hshnju@gmail.com>
2017-01-02 20:48:49 +0800

    upload

--- src/main/java/com/mycompany/app/HibernateSessionManager.java
+++ src/main/java/com/mycompany/app/HibernateSessionManager.java
@@ -1,31 +1,27 @@
 package com.mycompany.app;
 
-import org.hibernate.HibernateException;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
-
 /**
  * Created by AirShaos on 24/12/16.
  */
 public class HibernateSessionManager {
-    private static final SessionFactory sessionFactory = buildSessionFactory();
-
-    private static SessionFactory buildSessionFactory() {
-        try {
-//            return new Configuration().configure().buildSessionFactory();
-            return new AnnotationConfiguration().configure().addAnnotatedClass(User.class).buildSessionFactory();
-        }catch (HibernateException ex){
-            System.err.println("SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-
-    public static void shutdown() {
-        // Close caches and connection pools
-        getSessionFactory().close();
-    }
+//    private static final SessionFactory sessionFactory = buildSessionFactory();
+//
+////    private static SessionFactory buildSessionFactory() {
+////        try {
+//////            return new Configuration().configure().buildSessionFactory();
+////            return new AnnotationConfiguration().configure().addAnnotatedClass(User.class).buildSessionFactory();
+////        }catch (HibernateException ex){
+////            System.err.println("SessionFactory creation failed." + ex);
+////            throw new ExceptionInInitializerError(ex);
+////        }
+////    }
+//
+//    public static SessionFactory getSessionFactory() {
+//        return sessionFactory;
+//    }
+//
+//    public static void shutdown() {
+//        // Close caches and connection pools
+//        getSessionFactory().close();
+//    }
 }
--- src/main/java/com/mycompany/app/Run.java
+++ src/main/java/com/mycompany/app/Run.java
@@ -1,25 +1,59 @@
 package com.mycompany.app;
 
-import org.hibernate.Session;
-
-import java.util.Date;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
 
 /**
  * Created by AirShaos on 24/12/16.
  */
 public class Run {
+    private static final String PERSISTENCE_UNIT_NAME = "HelloWorldPU";
+    private static EntityManagerFactory factory;
+    
     public static void main(String[] args){
-        Session session = HibernateSessionManager.getSessionFactory().openSession();
-        session.beginTransaction();
-        User user = new User();
+//        Session session = HibernateSessionManager.getSessionFactory().openSession();
+//        session.beginTransaction();
+//        User user = new User();
+//
+////        user.setUserId(1);
+//        user.setUsername("James");
+//        user.setCreatedBy("Application");
+//        user.setCreatedDate(new Date());
+//        System.out.println("-------for fun 2");
+//
+//        session.save(user);
+//        session.getTransaction().commit();
+      
+      
+//        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+//        EntityManager em = factory.createEntityManager();
+//        em.getTransaction().begin();
+//
+//        User user = new User();
+//        user.setUsername("James1");
+//        user.setCreatedBy("Application");
+//        user.setCreatedDate(new Date());
+//
+//        em.persist(user);
+//        em.getTransaction().commit();
+//        em.close();
+  
+  
+        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+        em.getTransaction().begin();
 
-//        user.setUserId(1);
-        user.setUsername("James");
-        user.setCreatedBy("Application");
-        user.setCreatedDate(new Date());
-        System.out.println("-------for fun 2");
+        List<User> users = em.createQuery("select u from User u").getResultList();
+      System.out.println(users.size());
+      System.out.println(users.get(0).getUsername());
 
-        session.save(user);
-        session.getTransaction().commit();
+      users.get(0).setUsername("James shaos");
+
+      em.getTransaction().commit();
+      em.close();
+//
+      
     }
 }
